0fbe0ebc18fb1db5d6b35a96583ef14e31210803
Rémi PEILLET <remi-peillet@hotmail.fr>
2018-02-28 13:22:15 +0100

    Modification du fichier setup

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 setup(
     name='lib_Partage_BSS',
     version='1.0.0',
-    packages=['doc', 'test_unitaire',
-              'lib_Partage_BSS',
+    packages=['lib_Partage_BSS',
               'lib_Partage_BSS.utils', 'lib_Partage_BSS.exceptions', 'lib_Partage_BSS.models','lib_Partage_BSS.services',
               ],
     url='https://gitlab.univ-rennes1.fr/57NUM/libPythonBssApi',
