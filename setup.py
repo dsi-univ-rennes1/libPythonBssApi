6a853f3b010d8a7087ea53908c15f9571c43b134
S.Bonnegent <171115@bonnegent.fr>
2020-06-18 15:42:04 +0200

    explicitation des dépendances pour pip

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/dsi-univ-rennes1/libPythonBssApi',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=['xmljson', 'requests', 'wheel']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
